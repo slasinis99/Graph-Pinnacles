c89d0f1d5e2ebe0bcd200419b567c2d7bff7ec9d
slasinis99 <recon589@gmail.com>
2023-04-16 18:36:31 -0500

    draw graph list

--- GraphDraw.py
+++ GraphDraw.py
@@ -4,19 +4,16 @@ import GraphUtil as gu
 os.environ["PATH"] += os.pathsep + 'C:/Program Files/Graphviz/bin'
 
 def draw_graphs(graph_list: list):
-    graph_list = [gu.create_graph(5,'star2'), gu.create_graph(7,'bipartite4')]
     G = Graph()
-    for g in graph_list:
+    for j,g in enumerate(graph_list):
+        P = g.get_pinnacles()
         sG = Graph()
-        for n in g.node_list:
-            sG.node('n', f'{n.get_value()}')
-
-G = Graph()
-G.node('1', '1')
-G.node('2', '2')
-G.node('3', '3')
-G.node('4','4',color='red')
-
-G.edges(['12','13','23','14','24','34'])
-
-G.render(view=True)
+        for i,n in enumerate(g.node_list):
+            if n.value in P: col = 'red'
+            else: col = 'blue'
+            sG.node(name=f'{j},{i}',label=f'{n.value}', color=col)
+        for r in range(len(g.adjacency_matrix)):
+            for c in range(r+1,len(g.adjacency_matrix[r])):
+                if g.adjacency_matrix[r][c] != 0: sG.edge(f'{j},{r}',f'{j},{c}')
+        G.subgraph(sG)
+    G.render(view=True)
