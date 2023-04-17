7e6905c191044e9da10e0f9cbc90db8318177507
slasinis99 <recon589@gmail.com>
2023-04-16 19:54:53 -0500

    minor

--- working_environment.py
+++ working_environment.py
@@ -5,6 +5,6 @@
 import GraphUtil as gu
 import GraphDraw as gd
 
-total,g_list = gu.pinnaclus_utopius(gu.create_graph(10,'wheel'),[10,8,7],True,complete=True)
+total,g_list = gu.pinnaclus_utopius(gu.create_graph(9,'wheel'),[9,8,7],True,complete=True)
 print(total)
 gd.draw_graphs(g_list,100)
